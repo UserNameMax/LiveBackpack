af8d5c5fd20e1c5427f944032218953d3ddcc21c
Maksim Mishenko <kirova32a@mail.ru>
2023-06-15 18:04:12 +0600

    [+] add allure annotations

--- src/test/kotlin/BackpackTest.kt
+++ src/test/kotlin/BackpackTest.kt
@@ -2,12 +2,16 @@ import domain.Logic
 import domain.impl.backpack.Backpack
 import domain.impl.backpack.BackpackFactory
 import domain.impl.backpack.Staf
+import io.qameta.allure.Allure
+import io.qameta.allure.Description
+import io.qameta.allure.Step
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class BackpackTest {
     @Test
+    @Description(value = "Проверка решение простой задачи о рюкзаке")
     fun solveSampleTask() {
         val weigh = 4
         val staf = listOf(
@@ -22,6 +26,7 @@ class BackpackTest {
     }
 
     @Test
+    @Description(value = "Проверка ограничение рюкзака на вес")
     fun fullBackpackTest() {
         val weigh = 100
         val staf = listOf(
@@ -34,6 +39,7 @@ class BackpackTest {
     }
 
     @Test
+    @Description(value = "Проверка некоректного условия")
     fun emptyBackpack(){
         val weigh = 5
         val staf = listOf(
@@ -44,12 +50,14 @@ class BackpackTest {
         val solve = runLive(weigh, staf)
         assertTrue(solve.insideStaf.isEmpty())
     }
-
+    @Step("Решение задачи о рюкзаке ")
     fun runLive(weigh: Int, stafs: List<Staf>): Backpack {
         val logic = Logic(BackpackFactory(weigh, stafs))
         for (i in 1..4) {
             logic.live()
         }
-        return (logic.population.minOfOrNull { it } as? Backpack) ?: Backpack(weigh, listOf(), listOf())
+        val result = (logic.population.minOfOrNull { it } as? Backpack) ?: Backpack(weigh, listOf(), listOf())
+        Allure.addAttachment("Результат", "text/plain", result.toString())
+        return result
     }
 }
