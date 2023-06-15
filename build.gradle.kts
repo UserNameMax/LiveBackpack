ec0edcc787da26cf502c63b32d05d7c2640452bd
Maksim Mishenko <kirova32a@mail.ru>
2023-06-15 17:33:21 +0600

    [+] add allure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,26 @@
 plugins {
     kotlin("jvm") version "1.8.20"
     application
+    id("io.qameta.allure") version "2.9.4"
 }
 
 group = "ru.omgtu.ivt213"
 version = "1.0-SNAPSHOT"
+
+allure {
+    report {
+        version.set("2.18.1")
+    }
+    adapter {
+        autoconfigure.set(true)
+        aspectjWeaver.set(true)
+        frameworks {
+            junit5 {
+                adapterVersion.set("2.18.1")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -12,14 +28,37 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("commons-io:commons-io:2.6")
+    implementation("io.qameta.allure:allure-java-commons:2.14.0")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    implementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
+tasks.withType(JavaCompile::class) {
+    sourceCompatibility = "${JavaVersion.VERSION_1_8}"
+    targetCompatibility = "${JavaVersion.VERSION_1_8}"
+    options.encoding = "UTF-8"
+}
+
+tasks.withType(Test::class) {
+    ignoreFailures = true
+    useJUnitPlatform {
+
+    }
+    systemProperty("junit.jupiter.execution.parallel.enabled", "true")
+    systemProperty("junit.jupiter.execution.parallel.config.strategy", "dynamic")
+
+    systemProperty("junit.jupiter.extensions.autodetection.enabled", "true")
+}
+
 kotlin {
-    jvmToolchain(18)
+    jvmToolchain(11)
 }
 
 application {
